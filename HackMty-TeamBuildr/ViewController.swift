f8de041630f046ef16442bb838ef49e644659e70
JimenaLomeli <jimena@lomeli.mx>
2018-08-25 17:23:01 -0500

    First commit

---
+++ HackMty-TeamBuildr/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  HackMty-TeamBuildr
+//
+//  Created by Jimena Lomeli on 8/25/18.
+//  Copyright © 2018 Jimena Lomeli. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
